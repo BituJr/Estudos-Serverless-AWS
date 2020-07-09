d4f90b8a1776737c8d947589d3b3dceca7bc1f0b
BituJr <ivanbbj@gmail.com>
2020-07-09 14:28:01 -0300

    Validações inteligentes com Joi e DynamoDB stream events

---
+++ demo04-trigger-dynamodb/trigger-dynamo/src/util/decoratorValidator.js
@@ -0,0 +1,21 @@
+const decoratorValidator = (fn, schema, argsType) => {
+    return async function (event) {
+         const data = JSON.parse(event[argsType])
+         // abortEarly == mostra todos os erros de uma vez
+         const { error, value } = await schema.validate(
+             data, { abortEarly: true }
+         )
+        // isso faz alterar a instancia de argumentos
+        event[argsType] = value
+        // argumentos serve para pegar todos os argumentos que vieram na funcao
+        // e mandar para frente
+        // o apply vai retornar a função que será executada posteriormente
+        if(!error) return fn.apply(this, arguments)
+
+        return {
+            statusCode: 422, // unprocessable entity,
+            body: error.message
+        }
+    }
+}
+module.exports = decoratorValidator

04914ea36ee3a633836702b0fa4ac1d1ac4f5533
BituJr <ivanbbj@gmail.com>
2020-07-09 14:22:46 -0300

    Introdução ao Serverless framework

---
+++ demo02-sls-framework/run.sh
@@ -0,0 +1,20 @@
+// installar o serverless
+npm i -g serverless
+
+// sls inicializar o serverless
+sls 
+
+// sempre fazer deploy antes de tudo para verificar se está com ambiente ok
+sls deploy 
+
+// invocar na AWS
+sls invoke -f hello --log
+
+// invocar local
+sls invoke local -f hello --log
+
+// configurar dashboard
+sls 
+
+// logs 
+sls logs -f hello -t 
